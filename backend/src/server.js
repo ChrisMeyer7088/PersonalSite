1616687bba2b7c98354ddb1ccd1325da3738c4bb
ChrisMeyer7088 <chrismeyer175@gmail.com>
2021-02-11 12:41:16 -0800

    Scadffolded project

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+app.use(morgan(':method :url :status :response-time ms'));
+app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+const server = app.listen(parseInt(process.env.PORT || 3000, 10));
+
+app.get('/', (req, res) => res.render('frontend', {
+  FRONTEND_ASSET_PATH: 'http://localhost:3001'
+}));
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+  });
+});
